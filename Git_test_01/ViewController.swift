1d319cffe30b5c050e7e0179c09250cba4552bdb
ChenJianglin <1215999200@qq.com>
2017-10-26 16:17:44 +0800

    commit by chenjianglin

--- Git_test_01/ViewController.swift
+++ Git_test_01/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let myString = "hello, world";
+        print(myString)
     }
 
     override func didReceiveMemoryWarning() {
